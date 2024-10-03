4d69aa1e620f4653d37db65dfc2377dda5adac26
ScytheSploits1 <hellnooh813@gmail.com>
2024-10-03 21:41:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 let keys = [];
 
+// Load keys from local storage
+function loadKeys() {
+    const storedKeys = localStorage.getItem('keys');
+    if (storedKeys) {
+        keys = JSON.parse(storedKeys);
+    }
+    updateKeyList();
+}
+
+// Save keys to local storage
+function saveKeys() {
+    localStorage.setItem('keys', JSON.stringify(keys));
+}
+
+// Add a new key
 function addKey() {
     const newKey = document.getElementById('newKey').value;
     if (newKey) {
         keys.push(newKey);
         document.getElementById('newKey').value = '';
+        saveKeys();
         updateKeyList();
     }
 }
 
+// Update the key list display
 function updateKeyList() {
     const keyList = document.getElementById('keyList');
     keyList.innerHTML = '';
@@ -18,3 +35,6 @@ function updateKeyList() {
         keyList.appendChild(li);
     });
 }
+
+// Load keys when the page loads
+window.onload = loadKeys;
